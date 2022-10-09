ccfc64c3dc1d8d5b5612ba76eeda34bdde055b1b
Yevhen Cherednyk <yevgen.cherednyk@gmail.com>
2022-10-09 15:49:40 +0300

    add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryItemsConteiner = document.querySelector('.gallery');
+const itetemsGallaryMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryItemsConteiner.insertAdjacentHTML('beforeend', itetemsGallaryMarkup);
+galleryItemsConteiner.addEventListener('click', onGalleryItemClick);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    )
+    .join('');
+}
+
+function onGalleryItemClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+
+  gallery.on('show.simplelightbox');
+}
